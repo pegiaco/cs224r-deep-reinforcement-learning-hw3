5eef4867416ebba2e16a7e6362095fcabe82d4e7
pegiaco <pegiacomelli@gmail.com>
2023-05-04 11:55:02 -0700

    Problem 2 first try

--- hw3/cs224r/critics/cql_critic.py
+++ hw3/cs224r/critics/cql_critic.py
@@ -84,7 +84,10 @@ class CQLCritic(BaseCritic):
         # Hint: After calculating cql_loss, augment the loss appropriately
         
         ### YOUR CODE HERE ###
-        loss = None
+        loss, qa_t_values, q_t_values = self.dqn_loss(ob_no, ac_na, next_ob_no, reward_n, terminal_n)
+        qa_t_logsumexp = torch.logsumexp(qa_t_values / self.cql_alpha, dim=1)
+        cql_loss = (self.cql_alpha * qa_t_logsumexp - q_t_values).mean()
+        loss += cql_loss
         
         ### YOUR CODE HERE ###
         self.optimizer.zero_grad()
@@ -94,9 +97,9 @@ class CQLCritic(BaseCritic):
         
         info = {'Training Loss': ptu.to_numpy(loss)}
         # TODO: Uncomment these lines after implementing CQL
-        # info['CQL Loss'] = ptu.to_numpy(cql_loss)
-        # info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
-        # info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
+        info['CQL Loss'] = ptu.to_numpy(cql_loss)
+        info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
+        info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
         
         self.learning_rate_scheduler.step()
 
