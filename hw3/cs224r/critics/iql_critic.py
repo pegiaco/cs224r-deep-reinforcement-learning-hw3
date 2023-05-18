2013aad1ed3126689874ced3c57995509dcc6f48
pegiaco <pegiacomelli@gmail.com>
2023-05-17 20:45:00 -0700

    final run

--- hw3/cs224r/critics/iql_critic.py
+++ hw3/cs224r/critics/iql_critic.py
@@ -69,7 +69,7 @@ class IQLCritic(BaseCritic):
         # aggregate it later
         ### YOUR CODE HERE ###
         tau = self.iql_expectile
-        loss = torch.where(diff >= 0, (1 - tau) * diff**2, tau * diff**2).mean()
+        loss = torch.where(diff <= 0, (1 - tau) * diff**2, tau * diff**2).mean()
         return loss
         ### YOUR CODE HERE ###
 
